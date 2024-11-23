2eb8e06430db30267bc7870067891befeaa923c8
josen <josen@compu_ignacio>
2024-11-22 23:37:14 -0300

    v0.3__jose nuñez 22-11-2024 23:27

--- src/ConexionDB/Conexion.java
+++ src/ConexionDB/Conexion.java
@@ -3,13 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ConexionDB;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 /**
  *
  * @author Vicente
  */
 public class Conexion {
+    public class ConexionBD {
+    
+    public Connection obtenerConexion()
+    {
+        Connection conexion = null;
+        //trycatch + tab
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/libreria", "root", "duoc.2024");
+            System.out.println("Conexion exitosa");
+        } catch (Exception e) {
+            System.out.println("Fallo conexión con Servidor");
+        }
+        
+        return conexion;
+    }
+    }
     
     
-    
-}
+
